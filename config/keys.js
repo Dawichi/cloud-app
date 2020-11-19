a468980d381ea834cb16ec71fcc52ad3bb1ee221
David M. Fajardo <davidmf99@gmail.com>
2020-11-19 19:13:15 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://dmfajardo:abc123..@mycloudappcluster.5vlw0.mongodb.net/MyCloudAppCluster?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
